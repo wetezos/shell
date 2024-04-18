17ca58a965214e5d29034f85fed97f37ee620e8e
wetezos <40154568+wetezos@users.noreply.github.com>
2024-04-18 15:08:20 +0800

    Update node-exporter.sh

--- node-exporter.sh
+++ node-exporter.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# 下载 Prometheus 和 Node Exporter
-wget https://github.com/prometheus/prometheus/releases/download/v2.51.2/prometheus-2.51.2.linux-amd64.tar.gz
-tar -xf prometheus-2.51.2.linux-amd64.tar.gz
+# 下载node_exporter并解压
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+tar -xf node_exporter-1.7.0.linux-amd64.tar.gz
 
-# 移动 Node Exporter 到 /usr/local/bin/
-mv ./prometheus-2.51.2.linux-amd64/node_exporter /usr/local/bin/
+# 将node_exporter二进制文件移动到/usr/local/bin/
+mv ./node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/
 
-# 删除下载的文件
-rm -rf prometheus-2.51.2.linux-amd64 prometheus-2.51.2.linux-amd64.tar.gz
+# 清理下载的文件
+rm -rf node_exporter-1.7.0.linux-amd64 node_exporter-1.7.0.linux-amd64.tar.gz
 
-# 写入服务文件
+# 将配置写入文件
 cat <<EOF > /etc/systemd/system/node_exporter.service
 [Unit]
 Description=node_exporter
@@ -27,8 +27,10 @@ RestartSec=10
 WantedBy=multi-user.target
 EOF
 
-# 启用 node_exporter 服务
+# 启用node_exporter服务
 systemctl enable node_exporter
 
-# 重启 node_exporter 服务
+# 重启node_exporter服务
 service node_exporter restart
+
+echo "node_exporter安装并配置完成！"
